476fbe2d800ae2c4f5e4ca3e68f2ae5f9e28b17f
Aleksandr Karelin <alexander.karelin@videinfra.com>
2026-05-09 21:23:26 +0700

    Add basic test for ProductController product list endpoint

---
+++ tests/Controller/ProductControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProductControllerTest extends WebTestCase
+{
+    public function testProductListReturns200AndShowProduct()
+    {
+        static::createClient()
+            ->request('GET', '/products');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h3', 'product 0');
+    }
+}
